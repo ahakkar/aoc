117750178ee0b9db839378087b9ed0da7b00f232
Antti Hakkarainen <a.hakkarainen@iki.fi>
2022-11-09 15:43:37 +0200

    05b with diagonals

--- 2021/05/aoc05b.py
+++ 2021/05/aoc05b.py
@@ -16,8 +16,8 @@ class Lines:
         self.lines: list = []
         # we could use 0 here for max_x and max_y, and inspect all the
         # numbers in add_lines_to_list, but it doubles the runtime
-        self.max_x: int = 10
-        self.max_y: int = 10
+        self.max_x:int = 1000
+        self.max_y:int = 1000
         
         self.read_file(filename)
         self.add_lines_to_list()          
@@ -47,12 +47,12 @@ class Lines:
         for row in self.data:
             if row != "":
                 temp = row.split(' -> ')
-                #x, y = [int(i) for i in temp[0].split(',')]
-                #if x > self.max_x: self.max_x = x 
-                #if y > self.max_y: self.max_y = y   
-                #x, y = [int(i) for i in temp[1].split(',')]
-                #if x > self.max_x: self.max_x = x 
-                #if y > self.max_y: self.max_y = y      
+                x, y = [int(i) for i in temp[0].split(',')]
+                if x > self.max_x: self.max_x = x 
+                if y > self.max_y: self.max_y = y   
+                x, y = [int(i) for i in temp[1].split(',')]
+                if x > self.max_x: self.max_x = x 
+                if y > self.max_y: self.max_y = y      
                 self.lines.append((temp[0], temp[1]))
                 
     def print_lines(self) -> None:
@@ -64,21 +64,50 @@ class Lines:
         x1, y1 = [int(i) for i in line[0].split(',')]    
         x2, y2 = [int(i) for i in line[1].split(',')]
         
-        # swap drawing dir from left to right if neccessary
-        if x1 > x2:                
-            x1, x2 = x2, x1  
-        # swap drawing dir from top to bot if neccessary
-        if y1 > y2:
-            y1, y2 = y2, y1    
-        
         # horizontal or vertical line
         if x1 == x2 or y1 == y2: 
+            # swap drawing dir from left to right if neccessary
+            if x1 > x2:                
+                x1, x2 = x2, x1  
+            # swap drawing dir from top to bot if neccessary
+            if y1 > y2:
+                y1, y2 = y2, y1    
+            
             # "draw" the line to 2d array
-            self.Matrix[y1:y2+1 , x1:x2+1] +=1              
+            self.Matrix[y1:y2+1 , x1:x2+1] +=1  
+                       
         #diagonal line
         else:   
-            print("line not drawn: diagonal")         
-            pass   
+            #print(x1, y1, "/", x2, y2)
+            #diagonal top-bot r-l
+            if x1 > x2 and y2 > y1:     
+                dist = x1-x2                
+                for i in range(0, dist+1):
+                    self.Matrix[y1:y1+1 , x1:x1+1] +=1
+                    x1 -=1
+                    y1 +=1            
+            #diagonal top-bot l-r
+            elif x2 > x1 and y2 > y1:
+                dist = x2-x1  
+                for i in range(0, dist+1):
+                    self.Matrix[y1:y1+1 , x1:x1+1] +=1
+                    x1 +=1
+                    y1 +=1
+            #diagonal bot-top r-l
+            elif x1 > x2 and y1 > y2:
+                dist = x1-x2   
+                for i in range(0, dist+1):
+                    self.Matrix[y1:y1+1 , x1:x1+1] +=1
+                    x1 -=1
+                    y1 -=1                
+            #diagonal bot-top l-r
+            else:
+                dist = x2-x1 
+                for i in range(0, dist+1):
+                    self.Matrix[y1:y1+1 , x1:x1+1] +=1
+                    x1 +=1
+                    y1 -=1 
+                 
             
     def print_matrix(self) -> None:
         """
@@ -101,32 +130,30 @@ class Lines:
         """        
         return numpy.count_nonzero(self.Matrix > 1)
     
-    def resize_array(self, size_x:int, size_y:int) -> None:
+    # DOES NOT WORK i have no idea what to do.
+    def reshape_array(self, size_x:int, size_y:int) -> None:
         """
         param : TODO
         return: none
         """
         
-        temp = numpy.zeros(size_x, size_y) 
-        #self.Matrix = numpy.resize(self.Matrix, (size_x, size_y))
+        self.Matrix = numpy.reshape(self.Matrix, (size_x, size_y))
         self.max_x = size_x
         self.max_y = size_y
  
 def main():
-    lines = Lines("simple.input")    
+    lines = Lines("input.txt")    
     list_of_lines = lines.get_lines()
-    
-    #lines.draw_line(list_of_lines[0])
-    #lines.draw_line(list_of_lines[1])
-    #lines.draw_line(list_of_lines[2])
-    #lines.draw_line(list_of_lines[3])
+        
+    #lines.draw_line(list_of_lines[1]) #diagonal bot-top l-r
+    #lines.draw_line(list_of_lines[5]) #diagonal bot-top r-l
+    #lines.draw_line(list_of_lines[8]) #diagonal top-bot l-r
+    #lines.draw_line(list_of_lines[9]) #diagonal bot-top l-r  
     
     for line in list_of_lines:
         lines.draw_line(line)
     
-    lines.print_matrix()
-    lines.resize_array(50,50)   
-    lines.print_matrix() 
+    #lines.print_matrix() 
     
     overlap = lines.calculate_overlap()
     print("overlap:", overlap)
