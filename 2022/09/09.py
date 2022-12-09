bf2b00d5cfd7340eda28fe801bbcd3bd7c1fe899
Antti Hakkarainen <a.hakkarainen@iki.fi>
2022-12-09 13:05:32 +0200

    putting coords to set gives too low result

--- 2022/09/09.py
+++ 2022/09/09.py
@@ -35,32 +35,26 @@ class Solution:
         self.__data:list = data
         self.__moves = len(self.__data)
         self.__moves_done = 0
-        self.h_pos = (0, 0)
+        self.h_pos = (1, 1)
+        self.t_pos = (1, 1)
         self.h_prev = ()
-        self.t_pos = (0, 0)
-        self.t_moved = 0
-
-    def print(self):
-        for row in self.mapx:
-            print(''.join(row))
-        
+        self.t_moves:list = set(self.t_pos)
+       
     def walk(self):                      
         while True:
             # return when all moves have been done
             if self.__moves_done == self.__moves:
-                print("silver:", self.t_moved)
+                print("Final position: H", self.h_pos, "T", self.t_pos)
+                print("silver:", len(self.t_moves))
                 return  
-            
-            #cmd = input("Cmd: ")
-            #if cmd != "":
-            #    break            
-            
+
             self.move()
     
     def move(self):
         if self.__moves_done < self.__moves:
             dir:str = self.__data[self.__moves_done][0:1]
             amt:int = int(self.__data[self.__moves_done][2:])
+            #print("command:", self.__data[self.__moves_done])
             
             i:int = 0
             while i < amt:  
@@ -68,26 +62,24 @@ class Solution:
                 if abs(self.h_pos[0]-self.t_pos[0]) > 1 or \
                    abs(self.h_pos[1]-self.t_pos[1]) > 1:
                     self.t_pos = self.h_prev  
-                    self.t_moved += 1                    
+                    self.t_moves.add(self.t_pos)                    
           
-                print("H", self.h_pos, "T", self.t_pos)   
+                #print("H", self.h_pos, "T", self.t_pos, "moves:", len(self.t_moves))   
                 
                 # save previous point
                 self.h_prev = self.h_pos          
 
                 if   dir == "R":  self.h_pos = (self.h_pos[0],  self.h_pos[1]+1)                            
                 elif dir == "L":  self.h_pos = (self.h_pos[0],  self.h_pos[1]-1)                       
-                elif dir == "U":  self.h_pos = (self.h_pos[0]-1,self.h_pos[1])                              
-                elif dir == "D":  self.h_pos = (self.h_pos[0]+1,self.h_pos[1])
+                elif dir == "U":  self.h_pos = (self.h_pos[0]+1,self.h_pos[1])                              
+                elif dir == "D":  self.h_pos = (self.h_pos[0]-1,self.h_pos[1])
 
-                #time.sleep(0.5)
-                #input("waiting..")
                 i += 1
             
             self.__moves_done += 1       
 
 def main():
-    # 9038 too much
+    # 9038 too much # 6174 too low
     data:list = read_file("D:\\GDrive\\Prog\\aoc\\2022\\09\\puzzle.input") 
     
     sol = Solution(data)  
