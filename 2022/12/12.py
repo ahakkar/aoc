5c565223e47710e4921ef7a6c9e86a9de8acafb9
Antti Hakkarainen <a.hakkarainen@iki.fi>
2023-11-13 19:00:35 +0200

    2022/12 read data to graph

--- 2022/12/12.py
+++ 2022/12/12.py
@@ -1,3 +1,12 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   12.py
+@Time    :   12/12/2022, 10:23:56
+@Author  :   Antti Hakkarainen
+@Task    :   Advent of Code 2022
+@Desc    :   TEMP
+'''
+
 def read_file(filename:str) -> list:
     """
     param : str, filename to read
@@ -19,21 +28,79 @@ def read_file(filename:str) -> list:
         
     return data;
 
-class Solution(object):
-    def __init__(self, data:list):
-        self.__data:list = data
+class Node:
+    def __init__(self, y, x, dist):
+        self.y = y
+        self.x = x
+        self.dist = dist
+        
+def print_map(heightmap):
+    for row in heightmap:
+        print(row)
 
-    def silver(self):
-        pass
+def find_src(source, heightmap):
+    # Find the Source "S"
+    for y in range(len(heightmap)):
+        for x in range(len(heightmap[y])):
+            if heightmap[y][x] == 'S':
+                print("src fnd")
+                source.y = y
+                source.x = x
+                return source
+
+def main():    
+    data:list = read_file("D:\\GDrive\\Prog\\aoc\\2022\\12\\simple.input") 
+    heightmap:list = []
+    for row in data:
+        heightmap.append([*row])
+        
+    source = Node(0, 0, 0)
+    source = find_src(source, heightmap)
+    #print(source.x, source.y)
     
-    def gold(self):
-        pass
-
-
-def main():
-    data:list = read_file("D:\\GDrive\\Prog\\aoc\\2022\\11\\simple.input") 
+    visited = [[False for _ in range(len(heightmap[0]))] for _ in range(len(heightmap))]
+    visited[source.x][source.y] = True
     
-    sol = Solution(data)
+    queue = [source]
+
+    is_index_valid = lambda list, index: -1 < index < len(list)
+    
+    while len(queue) > 0:
+        source = queue.pop(0)
+        y = source.y
+        x = source.x
+        dist = source.dist
+
+        if (heightmap[y][x] == 'E'):
+            print(source.dist)
+            break
+        
+        # moving up
+        if is_index_valid(heightmap, y-1) and visited[y][x]:
+            queue.append(Node(y - 1, x, dist + 1))
+            visited[y - 1][x] = True
+ 
+        # moving down
+        if is_index_valid(heightmap, y+1) and visited[y][x]:
+            queue.append(Node(y + 1, x, dist + 1))
+            visited[y + 1][x] = True
+ 
+        # moving left
+        if is_index_valid(heightmap[y], x-1) and visited[y][x]:
+            queue.append(Node(y, x-1, dist + 1))
+            visited[y][x-1] = True
+ 
+        # moving right
+        if is_index_valid(heightmap[y], x+1) and visited[y][x]:
+            queue.append(Node(y, x+1, dist + 1))
+            visited[y][x+1] = True
+        
+        #for i, j in [(0,1), (0,-1), (1,0), (-1,0)]:
+        #    if is_index_valid(heightmap, y+i) and is_index_valid(heightmap[y+i], x+j):
+        #        if not_visited[y][x]:
+        #            queue.append(Node_item(source.y + i, source.x + j, source.dist + 1))
+        #            not_visited[y+i][x+j] = False      
+
     return 0
 
 if __name__ == "__main__":
---
+++ 2022/12/task12v3.py
@@ -0,0 +1,97 @@
+"""
+Anew go with A* algorithm learned from Tiraka1 in fall 2023
+"""
+
+def main():
+    """_summary_ runs the program
+    """
+    graph = Graph()
+    graph.create_graph_from_2d_array("simple.input")
+
+class Graph:
+    """_summary_ Graph class for A* algorithm
+    """
+    def __init__(self):
+        self._start = None  # Initialize start
+        self._end = None    # Initialize end
+        self._max_width = 0
+        self._max_height = 0
+        
+    def read_file(self, filename: str) -> list:
+        """
+        Reads lines from a given filename.
+        """
+        try:
+            with open(filename, "r", encoding="utf-8") as read_file:
+                return read_file.read().splitlines()
+        except FileNotFoundError:
+            print(f"Bad file name! {filename}")
+
+        return []
+
+    def create_graph_from_2d_array(self, filename: str):
+        """_summary_ creates a graph from a 2d array
+
+        :param str filename: _description_
+        """
+        data = self.read_file(filename)
+        self._max_height = len(data)
+        self._max_width = len(data[0])
+        graph:dict[tuple, tuple] = {}
+
+        for y, row in enumerate(data):
+            for x, char in enumerate(row):
+                if char == "S":
+                    self._start = (x, y)
+                    char = "a"
+                elif char == "E":
+                    self._end = (x, y)
+                    char = "z"
+
+                graph[(y, x)] = (ord(char) - 96, [])
+       
+        for key, value in graph.items():
+            #print(key, value)
+            adjacentPositions = self.getAdjacentPositions(key)
+            for pos in adjacentPositions:
+                #print(f"height at key{key}: {value[0]}, height at pos {pos} {graph[pos][0]}")
+                if self.isValidEdge(value[0], graph[pos][0]):      
+                    graph[key][1].append(pos)                     
+
+        print(graph)
+
+    def getAdjacentPositions(self, key:tuple) -> list:
+        """_summary_
+        Returns a list of valid positions adjacent to the given 
+        position, checking for boundaries
+
+        :param tuple key: _description_
+        :return list: _description_
+        """
+        positions:list = []
+        
+        # legal directions
+        moves = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        
+        for dx, dy in moves:
+            new_x, new_y = key[0] + dx, key[1] + dy
+            
+            # boundary check
+            if 0 <= new_x < self._max_height and 0 <= new_y < self._max_width:
+                positions.append((new_x, new_y))
+        
+        
+        return positions
+
+    def isValidEdge(self, h1:int, h2:int) -> bool:
+        if h1 == h2:    # same height
+            return True
+        if h1+1 == h2:  # one step up
+            return True
+        if h2 > h1:     # any number of steps down
+            return True
+        return False
+
+
+if __name__ == "__main__":
+    main()
