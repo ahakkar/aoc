8d86acd452e2ce483a577f5c9a4c8c3dadf66025
Antti Hakkarainen <a.hakkarainen@iki.fi>
2023-12-03 23:01:27 +0200

    optimize file reading & data copying

--- 2023/rust/src/day03var.rs
+++ 2023/rust/src/day03var.rs
@@ -1,15 +1,5 @@
-// These warnings are reaally annoying while coding prototype stuff
-#![allow(unused_parens)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-#![allow(unused_mut)]
-#![allow(clippy::needless_return)]
-#![allow(clippy::needless_range_loop)]
-
-use std::collections::HashMap;
 use regex::Regex;
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::fs;
 use std::time::Instant;
 use lazy_static::lazy_static;
 
@@ -20,35 +10,21 @@ lazy_static! {
 
 fn main() {
     let start = Instant::now();
-    let data: Result<Vec<String>, io::Error> = read_file("input/03puzzle.txt");
 
-    if let Ok(lines) = data {
-        process(&lines);
-    }
+    let input = fs::read_to_string("input/03puzzle.txt").unwrap();
+    let data: Vec<&str> = input.lines().collect();
+    process(&data);
+    
     let duration = start.elapsed();
     println!("Time elapsed in main() is: {:?}", duration);
 }
 
-fn read_file(file_name: &str) -> io::Result<Vec<String>> {
-    let file = File::open(file_name)?;
-    let reader = BufReader::new(file);
-    let mut data: Vec<String> = vec![];
-
-    for line in reader.lines() {
-        let line = line?;
-        data.push(line);
-    }
-
-    Ok(data)
-}
-
 // find gears and check if they have neighbouring numbers
-fn process(data: &[String]) {    
+fn process(data: &[&str]) {    
     let mut sum:i64 = 0;
-    for (y, row) in data.iter().enumerate() {
-        let gear_iters = RE_SYMBOL.find_iter(row);   
-        for gear in gear_iters {
-            sum += check_neighbours(data, &y, &gear.start());            
+    for (row, row_str) in data.iter().enumerate() {
+        for gear in RE_SYMBOL.find_iter(row_str) {
+            sum += check_neighbours(data, &row, &gear.start());            
         }
     }  
     println!("{}", sum); // 84900879
@@ -59,35 +35,33 @@ fn process(data: &[String]) {
  *   8  9 10  * 12 13 14 15 
  *  16 17 18 19 20 21 22 23
  */
-fn check_neighbours(data: &[String], row: &usize, col: &usize) -> i64 {
-    let mut neighbours: String = String::new();    
+fn check_neighbours(data: &[&str], row: &usize, col: &usize) -> i64 {
     let start = col.saturating_sub(3);
     let end = start + 7;
+
+    let mut neighbours:String = String::new();    
+    let mut n1: i64 = 0;
+    let mut n2: i64 = 0;
 
     // iterate through rows -1, 0, 1
     for i in 0..3 {
         neighbours += &data[row + i -1][start..end];
         neighbours.push('|');
-    }    
+    } 
 
     // check if any numbers are next to the gear in the flattened view
-    let num_iters = RE_ID.find_iter(&neighbours);
-    let mut n1: i64 = 0;
-    let mut n2: i64 = 0;
-
-    for num in RE_ID.find_iter(&neighbours) {        
-        let start = num.start();
-        let end = num.end();
-
-        if (start..end).any(|index| [2,3,4,10,12,18,19,20].contains(&index)) {
+    for num in RE_ID.find_iter(&neighbours) {  
+        if (num.start()..num.end())
+            .any(|index| [2,3,4,10,12,18,19,20].contains(&index))
+        {
             let num_value = num.as_str().parse::<i64>().unwrap_or(-1);
-            if n1 == 0 {
-                n1 = num_value;
-            } else if n2 == 0 {
-                n2 = num_value;               
-            } 
+            match (n1, n2) {
+                (0, _) => n1 = num_value,
+                (_, 0) => n2 = num_value,
+                _ => (),
+            }
         }
     }
 
-    return n1*n2;
+    n1*n2
 }
--- 2023/rust/src/day04.rs
+++ 2023/rust/src/day04.rs
@@ -1,15 +1,12 @@
 // These warnings are reaally annoying while coding prototype stuff
 #![allow(unused_parens)]
-#![allow(unused_imports)]
 #![allow(unused_variables)]
 #![allow(unused_mut)]
 #![allow(clippy::needless_return)]
 #![allow(clippy::needless_range_loop)]
 
-use std::collections::HashMap;
 use regex::Regex;
-use std::fs::{File, self};
-use std::io::{self, BufRead, BufReader};
+use std::fs;
 use std::time::Instant;
 use lazy_static::lazy_static;
 
@@ -23,13 +20,13 @@ fn main() {
     let input = fs::read_to_string("input/03simple.txt").unwrap();
     let data: Vec<&str> = input.lines().collect();
 
-    process(data);
+    process(&data);
 
     let duration = start.elapsed();
     println!("Time elapsed in main() is: {:?}", duration);
 }
 
-fn process(data: Vec<&str>) {    
+fn process(data: &Vec<&str>) {    
     let mut sum:i64 = 0;
 
     for row in data {
