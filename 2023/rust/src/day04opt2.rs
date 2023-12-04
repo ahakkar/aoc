7623a2a0a7e87a376d5283ff089202e9f6d185c3
Antti Hakkarainen <a.hakkarainen@iki.fi>
2023-12-04 12:13:52 +0200

    04: save 1ms more with split instead of regex

--- 2023/rust/src/day04opt2.rs
+++ 2023/rust/src/day04opt2.rs
@@ -1,16 +1,5 @@
-#![allow(unused_variables)]
-#![allow(unused_mut)]
-
-use regex::Regex;
-use std::collections::HashSet;
 use std::fs;
 use std::time::Instant;
-use lazy_static::lazy_static;
-
-lazy_static! {
-    static ref RE_ID: regex::Regex = Regex::new(r"\d+").unwrap();
-    static ref RE_SYMBOL: regex::Regex = Regex::new(r"[*]").unwrap();
-}
 
 fn main() {
     let start = Instant::now();
@@ -26,32 +15,31 @@ fn main() {
 fn process(data: &[&str]) {  
     // Store counts of each card
     let mut counts = vec![1; data.len()];
-    data.iter().enumerate().for_each(|(i, row_str)| {
-        (0..process_score(row_str)).for_each(|n| counts[i + n + 1] += counts[i]);
-    });   
+
+    for (i, row_str) in data.iter().enumerate() {
+        for n in 0..process_score(row_str) {
+            counts[i + n + 1] += counts[i];
+        }
+    }   
 
     // Sum the count of all cards
-    println!("{}", counts.iter().sum::<usize>());
+    println!("{}", counts.iter().sum::<usize>()); // 5422730
 }
 
 // count how many wins each game has
 fn process_score(row_str: &str) -> usize {
-    // remove garbage from rows
-    let start = row_str.find(':').map_or(0, |pos| pos + 1);
-    let sliced_row = &row_str[start..];
-    let mut parts = sliced_row.split('|');
+    // discard all before ': ' in rows
+    let (winning_nums, lottery_nums) = 
+        row_str.split_once(": ").unwrap().1.split_once(" | ").unwrap();
 
-    let winning_nums: Vec<i8> = RE_ID.find_iter(parts.next().unwrap())
-        .filter_map(|mat| mat.as_str().parse::<i8>().ok())
-        .collect();
-    let lottery_nums: Vec<i8> = RE_ID.find_iter(parts.next().unwrap())
-        .filter_map(|mat| mat.as_str().parse::<i8>().ok())
-        .collect();     
-    
-    count_matching_numbers(&winning_nums, &lottery_nums)    
-}
+    let winning_nums = winning_nums
+        .split(' ')
+        .filter_map(|n| n.parse::<usize>().ok())
+        .collect::<std::collections::HashSet<_>>();
 
-fn count_matching_numbers(vec1: &[i8], vec2: &[i8]) -> usize {
-    let set1: HashSet<_> = vec1.iter().collect();
-    vec2.iter().filter(|&n| set1.contains(n)).count()
+    // Count and return the matching numbers between winning & lottery
+    lottery_nums.split(' ')
+        .filter_map(|n| n.parse::<usize>().ok())
+        .filter_map(|n| winning_nums.contains(&n).then_some(1))
+        .sum()
 }
