de0e4a52b924851abd7762a04f57af46fa1eaab0
Antti Hakkarainen <a.hakkarainen@iki.fi>
2023-12-06 13:57:55 +0200

    06 real understanding

--- 2023/rust/src/day06opt.rs
+++ 2023/rust/src/day06opt.rs
@@ -7,8 +7,7 @@ fn main() {
     println!("Time elapsed in main() is: {:?}", duration);
 }
 
-fn process() {       
-    println!("part1 test: {}", calc_dist(30, 200)); // 9 but results in 10 :D
+fn process() {   
     println!("part1: {}", 
         calc_dist(62, 553) * calc_dist(64, 1010) * 
         calc_dist(91, 1473) * calc_dist(90, 1074)); // 840336
@@ -17,11 +16,15 @@ fn process() {
 
 fn calc_dist(time: i64, limit: i64) -> i64 {
     let s = ((time*time-4*limit) as f64).sqrt();
-    let mut x0 = (((time as f64) - s) / 2.0) as i64;
-    let mut x1 = (((time as f64) + s) / 2.0) as i64;
+    let mut x0 = ((time as f64) - s) / 2.0;
+    let mut x1 = ((time as f64) + s) / 2.0;
 
-    if x0 == 0 { x0 = 1; } else { x0 += 1; }
-    if x1 == 0 { x1 -= 1}
+    // if for example x0 = 10 and x10= 20 (integer results)
+    if x0 == x0.ceil() { x0 += 1.0; } else { x0 = x0.ceil(); }
+    if x1 == x1.floor() { x1 -= 1.0 } else { x1 = x1.floor(); }
 
-    x1-x0 + 1    
+    x0 = x0.ceil();
+    x1 = x1.floor();
+
+    (x1-x0 + 1.0) as i64
 }
