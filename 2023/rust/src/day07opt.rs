cbfc48ad29fc3b96d9340c6eb22499ee1837a439
Antti Hakkarainen <a.hakkarainen@iki.fi>
2023-12-07 17:46:31 +0200

    07 tidy up code a bit

--- 2023/rust/src/day07opt.rs
+++ 2023/rust/src/day07opt.rs
@@ -3,13 +3,12 @@ use std::collections::HashMap;
 use std::fs;
 use std::time::Instant;
 
-static JOKER: i8 = 1;
 #[derive(Eq, PartialEq)]
 struct Hand {
     cards: Vec<i8>,    
     htype: i8,
     str: String,
-    bid: i16
+    bid: usize
 }
 
 // With equal cases compare indexes from beginning
@@ -41,41 +40,40 @@ impl Ord for Hand {
     }
 }
 
-// Converts the string to a vector of ints
+// Converts card symbols to int values for easier comparison
 fn strtoivec(string: &str) -> Vec<i8> {
-    let mut hand : Vec<i8> = vec![];
-    for card in string.chars() {  
-        hand.push(match card {
-            '2'..='9' => card as i8 - '0' as i8,
-            'A' => 14,
-            'K' => 13,
-            'Q' => 12,            
-            'T' => 10,
-            'J' => 1, // joker, not a jack 
-            _   => 0,
-        });        
-    }
-    hand
+    string.chars().map(|card| match card {
+        '2'..='9' => card.to_digit(10).unwrap() as i8,
+        'A' => 14,
+        'K' => 13,
+        'Q' => 12,            
+        'T' => 10,
+        'J' => 1, // joker, not a jack 
+         _  => panic!(),
+    }).collect()    
 }
 
 // count occurencies of each card and match their frequency
 fn calc_htype(cards: &[i8]) -> i8 {
-    let mut counts = HashMap::new();
+    let mut card_counts = HashMap::new();
     let mut joker_count = 0;
 
     for &card in cards {
-        if card == JOKER {
-            joker_count += 1;
-        } else {
-            *counts.entry(card).or_insert(0) += 1;
+        match card {
+            1 => joker_count += 1,
+            _ => *card_counts.entry(card).or_insert(0) += 1,
         }
     }
-    
+
     // Special case with full Joker hand
     if joker_count == 5 { return 7 }
 
     // For each joker, increase the amount of largest non-joker at hand by 1
-    let mut freqs: Vec<_> = counts.values().cloned().collect();
+    let mut freqs: Vec<_> = card_counts
+        .values()
+        .cloned()
+        .collect();
+
     freqs.sort_unstable_by(|a, b| b.cmp(a));
     
     if joker_count > 0 {
@@ -85,13 +83,13 @@ fn calc_htype(cards: &[i8]) -> i8 {
     }
 
     match freqs.as_slice() {
-        [5] => 7,               // Five of a Kind
-        [4, _] => 6,            // Four of a Kind
-        [3, 2] | [3, 2, 1] => 5,// Full House
-        [3, ..] => 4,           // Three of a Kind
-        [2, 2, ..] => 3,        // Two Pair
-        [2, ..] => 2,           // One Pair
-        _ => 1,                 // High Card
+        [5]         => 7, // Five of a Kind
+        [4, _]      => 6, // Four of a Kind
+        [3, 2]      => 5, // Full House
+        [3, ..]     => 4, // Three of a Kind
+        [2, 2, ..]  => 3, // Two Pair
+        [2, ..]     => 2, // One Pair
+        _           => 1, // High Card
     }
 }
 
@@ -112,7 +110,7 @@ fn process(data: &[&str]) {
     for row in data {
         let (a, b) = row.split_once(' ').unwrap();
         let hand_str = a.parse::<String>().unwrap();
-        let bid = b.parse::<i16>().unwrap();
+        let bid = b.parse::<usize>().unwrap();
 
         score.push(
             Hand {     
@@ -125,9 +123,10 @@ fn process(data: &[&str]) {
 
     score.sort();
 
-    let sum: usize = score.iter()
+    let sum: usize = score
+        .iter()
         .enumerate()
-        .map(|(i, hand)| hand.bid as usize * (i + 1))
+        .map(|(i, hand)| hand.bid * (i + 1))
         .sum();
 
     println!("sum: {}", sum);
