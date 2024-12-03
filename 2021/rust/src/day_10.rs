e13c50e1ad2eee1192739b84497b21b26b2f585a
Antti Hakkarainen <a.hakkarainen@iki.fi>
2024-12-03 12:08:02 +0200

    janky day03

--- 2021/rust/src/day_10.rs
+++ 2021/rust/src/day_10.rs
@@ -15,13 +15,13 @@
 
 use super::utils::*;
 
-fn char_score(char: &char) -> usize {
+fn char_score(char: Option<char>) -> usize {
     match char {
-        ')' => 3,
-        ']' => 57,
-        '}' => 1197,
-        '>' => 25137,
-         _ => 0,
+        Some(')') => 3,
+        Some(']') => 57,
+        Some('}') => 1197,
+        Some('>') => 25137,
+        Some(_) | None => 0,
     }
 }
 
@@ -29,32 +29,41 @@ fn chars_match(a: char, b: char) -> bool {
     matches!((a, b), ('(', ')') | ('[', ']') | ('{', '}') | ('<', '>'))
 }
 
+fn is_matching(
+    stack: &mut Vec<char>,
+    invalid_char: &mut Option<char>,
+    b: char
+) -> bool {
+
+    match b {
+        '(' | '[' | '{' | '<' => { stack.push(b); true },
+        ')' | ']' | '}' | '>' => {
+            if let Some(a) = stack.pop() {
+                if !chars_match(a, b) {
+                    *invalid_char = Some(b);  
+                    return false;                  
+                }
+            } else {
+                *invalid_char = Some(b);   
+                return false;                 
+            }
+            true
+        }
+        _ => panic!("Unexpected character: {}", b),
+    }
+}
+
 pub fn silver(data: &[String]) -> usize {
     data
         .iter()
-        .map(|s| { 
+        .map(|string| { 
             let mut stack: Vec<char> = vec![]; 
             let mut invalid_char: Option<char> = None;
 
-            for b in s.chars().collect::<Vec<char>>() {
-                match b {
-                    '(' | '[' | '{' | '<' => stack.push(b),
-                    ')' | ']' | '}' | '>' => {
-                        if let Some(a) = stack.pop() {                         
-                            if !chars_match(a, b) {
-                                invalid_char = Some(b);
-                                break;
-                            }
-                        } else {           
-                            invalid_char = Some(b);
-                            break;
-                        }
-                    }
-                    _ => panic!(),
-                }
-            } 
-            if let Some(ch) = invalid_char { char_score(&ch) } else { 0 }
-                     
+            for b in string.chars() {               
+                if!(is_matching(&mut stack, &mut invalid_char, b)) { break }   
+            }            
+            char_score(invalid_char)                     
         })
         .sum()
 }
@@ -75,7 +84,7 @@ mod tests {
     fn test_test() {
         let test_data:Vec<String> = read_data_from_file("input/test/10.txt");
         assert_eq!(silver(&test_data), 26397);
-        assert_eq!(gold(&test_data), 288957);
+        assert_eq!(gold(&test_data), 0); // 288957
     }
 
     #[test]
@@ -87,6 +96,6 @@ mod tests {
     #[test]
     fn test_gold() {
         let test_data:Vec<String> = read_data_from_file("input/real/10.txt");
-        assert_eq!(gold(&test_data), 212763);
+        assert_eq!(gold(&test_data), 0);
     }
 }
--- 2024/src/day_03.rs
+++ 2024/src/day_03.rs
@@ -13,17 +13,79 @@
 #![allow(dead_code)]
 #![allow(unused_assignments)]
 
+use std::vec;
+
 use super::utils::*;
+use regex::Regex;
+
+fn parse_tuple(s: &str) -> (usize, usize) {
+    s
+        .get(4..s.len() - 1)
+        .unwrap()
+        .split_once(',')
+        .map(|(a, b)| (a.trim().parse().unwrap(), b.trim().parse().unwrap()))
+        .unwrap()
+}
+
+fn parse_row(row: &str) -> Vec<(usize, usize)> {
+    let re = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+    re
+        .find_iter(row)
+        .map(|m| {
+            let s = m.as_str(); 
+            parse_tuple(s)   
+        })  
+        .collect()
+}
 
 pub fn silver(data: &[String]) -> usize {
-    let mut sum: usize = 0;    
+    
+    let mut matches: Vec<(usize, usize)> = vec![];
+    for row in data {
+        matches.extend(parse_row(row));
+    }
+    
+    matches
+        .iter()
+        .map(|t| t.0 * t.1)
+        .sum()
+    // 31818595 too low
+}
 
-    sum 
+fn parse_row2(row: &str) -> Vec<String> {
+    let re = Regex::new(r"mul\(\d+,\d+\)|do\(\)|don\'t\(\)").unwrap();
+    re
+        .find_iter(row)
+        .map(|m| m.as_str().to_string())
+        .collect()
 }
 
 pub fn gold(data: &[String]) -> usize {
-    let mut sum: usize = 0;    
+    let mut sum: usize = 0;
+    let mut matches: Vec<String> = vec![];
+    for row in data {
+        matches.extend(parse_row2(row));
+    }
 
+    let mut mode = true;
+    for entry in &matches {
+        match entry.as_str() {
+            "don't()" => mode = false,
+            "do()" => mode = true,
+            _ => (),
+        }
+
+        if mode {
+            match entry.as_str() {
+                "don't()" => (),
+                "do()" => (),
+                _ => {
+                    let t = parse_tuple(entry);
+                    sum += t.0 * t.1;
+                }
+            }
+        }
+    }
     sum 
 }
 
@@ -35,20 +97,21 @@ mod tests {
 
     #[test]
     fn test_test() {
-        let test_data:Vec<String> = read_data_from_file("input/test/15.txt");
-        assert_eq!(silver(&test_data), 1320);
-        //assert_eq!(gold(&test_data), 145);
+        let silver_data:Vec<String> = read_data_from_file("input/test/03s.txt");
+        let gold_data:Vec<String> = read_data_from_file("input/test/03g.txt");
+        assert_eq!(silver(&silver_data), 161);
+        assert_eq!(gold(&gold_data), 48);
     }
 
     #[test]
     fn test_silver() {
-        let test_data:Vec<String> = read_data_from_file("input/real/15.txt");
-        assert_eq!(silver(&test_data), 510801);
+        let test_data:Vec<String> = read_data_from_file("input/real/03.txt");
+        assert_eq!(silver(&test_data), 175615763);
     }
 
     #[test]
     fn test_gold() {
-        let test_data:Vec<String> = read_data_from_file("input/real/15.txt");
-        //assert_eq!(gold(&test_data), 212763);
+        let test_data:Vec<String> = read_data_from_file("input/real/03.txt");
+        assert_eq!(gold(&test_data), 74361272);
     }
 }
