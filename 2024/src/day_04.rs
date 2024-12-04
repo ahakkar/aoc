66728afd00e3c7c5af8e54f81f987f661d7028a3
Antti Hakkarainen <a.hakkarainen@iki.fi>
2024-12-04 15:57:04 +0200

    accidentally overwrited original 04 solution

--- 2024/src/day_04.rs
+++ 2024/src/day_04.rs
@@ -4,57 +4,98 @@
  * https://github.com/ahakkar/
 **/
 
+use regex::Regex;
+
 use super::utils::*;
 
-pub fn silver(data: &[String]) -> usize {
-    let mut sum: usize = 0;    
-    let map:GridMap<char> = GridMap::new(data_as_chars(data)); 
-    let height = data.len();
-    let width  = data[0].len();
+fn get_char(data: &[String], y: usize, x: usize) -> char {
+    data
+        .get(y)
+        .and_then(|row| row.get(x..x+1))
+        .unwrap()
+        .chars()
+        .next()
+        .unwrap()
+}
 
-    const PATTERN1: [char; 4] = ['X', 'M', 'A', 'S'];
-    const PATTERN2: [char; 4] = ['S', 'A', 'M', 'X'];
+fn flatten_data(data: &[String]) -> (String, String, String) {
+    let mut vert = String::new();
+    let mut r_dg = String::new();
+    let mut l_dg = String::new();
+    let (rows, cols) = (data.len(), data[0].len());
 
-    for y in 0..height {
-        for x in 0..width {    
-            if let Some(&idx) = map.get_idx(x, y) {
-                if idx != 'X' && idx != 'S' {
-                    continue;
-                }
-
-                for dir in [Direction::North, Direction::NorthEast, 
-                            Direction::East, Direction::SouthEast]
-                {
-                    let mut loc = Coord::new(x as isize, y as isize);
-                    let mut pat1_match = true;
-                    let mut pat2_match = true;
-
-                    for i in 0..4 {  
-                        // Reduce unneccessary loops                 
-                        if  loc.x < 0 || loc.x >= width as isize || 
-                            loc.y < 0 || loc.y >= height as isize
-                        {
-                            pat1_match = false;
-                            pat2_match = false;
-                            break;
-                        }
-
-                        // Reduce unneccessary loops  
-                        let c = map.get_cell(&loc).unwrap_or('_');
-                        if c != PATTERN1[i] { pat1_match = false }
-                        if c != PATTERN2[i] { pat2_match = false }            
-                        if !pat1_match && !pat2_match { break }
-    
-                        loc = loc.neighbour(dir);
-                    }
-
-                    if pat1_match || pat2_match {
-                        sum += 1;
-                    }                
-                }
-            }
+    for x in 0..cols {
+        for y in 0..rows {
+            vert.push(get_char(data, y, x));
         }
+        vert.push(' ');
     }
+
+    // Right diagonal
+    for col in 0..cols {
+        let mut x = col;
+        let mut y = 0;
+        while x < cols && y < rows {
+            r_dg.push(get_char(data, y, x));
+            x += 1;
+            y += 1;
+        }
+        r_dg.push(' ');
+    }
+    for row in 1..rows {
+        let mut x = 0;
+        let mut y = row;
+        while x < cols && y < rows {
+            r_dg.push(get_char(data, y, x));
+            x += 1;
+            y += 1;
+        }
+        r_dg.push(' ');
+    }
+
+
+    // Left diagonal
+    for col in (0..cols).rev() {
+        let mut x = col;
+        let mut y = 0;
+        while x < cols && y < rows {
+            l_dg.push(get_char(data, y, x));
+            if x == 0 { break; }
+            x -= 1;
+            y += 1;
+        }
+        l_dg.push(' ');
+    }
+    for row in 1..rows {
+        let mut x = cols - 1;
+        let mut y = row;
+        while x < cols && y < rows {
+            l_dg.push(get_char(data, y, x));
+            if x == 0 { break; }
+            x -= 1;
+            y += 1;
+        }
+        l_dg.push(' ');
+    }
+
+    (vert, r_dg, l_dg)
+}
+
+pub fn silver(data: &[String]) -> usize {
+    let f = Regex::new(r"XMAS").unwrap();
+    let b = Regex::new(r"SAMX").unwrap();
+    let mut sum: usize = 0;    
+    let horz = data.join(" ");
+    let (vert, dg_r, dg_l) = flatten_data(data);
+
+    sum += f.find_iter(&horz).count();
+    sum += b.find_iter(&horz).count();    
+    sum += f.find_iter(&vert).count();
+    sum += b.find_iter(&vert).count();   
+    sum += f.find_iter(&dg_r).count();
+    sum += b.find_iter(&dg_r).count();  
+    sum += f.find_iter(&dg_l).count();
+    sum += b.find_iter(&dg_l).count();  
 
     sum
 }
