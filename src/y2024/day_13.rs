99f5f216e4a08ed8b8396079b4df7d4c8c5cc239
Antti Hakkarainen <a.hakkarainen@iki.fi>
2024-12-13 13:03:24 +0200

    day 13b

--- src/y2024/day_13.rs
+++ src/y2024/day_13.rs
@@ -4,17 +4,7 @@
  * https://github.com/ahakkar/
 **/
 
-#![allow(dead_code)]
-#![allow(unused_parens)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-#![allow(unused_mut)]
-#![allow(unused_assignments)]
-#![allow(unused_must_use)]
-#![allow(clippy::needless_return)]
-#![allow(clippy::needless_range_loop)]
-#![allow(clippy::only_used_in_recursion)]
-
+use num_integer::lcm;
 use regex::Regex;
 
 use crate::{util::point::Point, Fro, Solution, TaskResult};
@@ -60,6 +50,7 @@ impl Fro for ClawContraption {
 
 // Main solvers
 impl Solution for ClawContraption {
+    // This is too slow for part Bc
     fn silver(&self) -> TaskResult {
         let a_weight = 3;
         let b_weight = 1;
@@ -99,10 +90,28 @@ impl Solution for ClawContraption {
         TaskResult::Usize(sum)
     }
 
+    // A mathematical problem for a person with no mathematical skills
+    // The solution is implemented based on MrQueasy's Kotlin solution
+    // Includes using linear algebra and Cramer's rule
     fn gold(&self) -> TaskResult {
         let a_weight = 3;
-        let b_weight = 1;
         let mut sum = 0;
+
+        for route in &self.data {
+            let m = lcm(route.a.x, route.a.y);
+            let xm = (m as f64) / (route.a.x as f64);
+            let ym = (m as f64) / (route.a.y as f64);
+            let px = (route.p.x + 10_000_000_000_000) as f64;
+            let py = (route.p.y + 10_000_000_000_000) as f64;
+
+            let b =
+                (px * xm - py * ym) / ((route.b.x as f64) * xm - (route.b.y as f64) * ym);
+            let a = (px - b * (route.b.x as f64)) / (route.a.x as f64);
+
+            if a.fract() == 0.0 && b.fract() == 0.0 {
+                sum += (a_weight as f64 * a + b) as i64;
+            }
+        }
 
         TaskResult::Usize(sum as usize)
     }
@@ -130,7 +139,7 @@ mod tests {
         let test_data = read_data_from_file("input/2024/test/13.txt");
         let queue = ClawContraption::fro(&test_data);
 
-        assert_eq!(queue.silver(), TaskResult::Usize(0));
+        assert_eq!(queue.silver(), TaskResult::Usize(480));
         assert_eq!(queue.gold(), TaskResult::Usize(0));
     }
 
@@ -139,7 +148,7 @@ mod tests {
         let real_data = read_data_from_file("input/2024/real/13.txt");
         let queue = ClawContraption::fro(&real_data);
 
-        assert_eq!(queue.silver(), TaskResult::Usize(0));
-        assert_eq!(queue.gold(), TaskResult::Usize(0));
+        assert_eq!(queue.silver(), TaskResult::Usize(35574));
+        assert_eq!(queue.gold(), TaskResult::Usize(80882098756071));
     }
 }
